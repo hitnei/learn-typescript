610155c26b2889b9ac6a6a8d4c8049159b30db15
nhi-hp <77150808+nhi-hp@users.noreply.github.com>
2021-05-15 22:26:29 +0700

    type val

--- 1.js
+++ 1.js
@@ -1,6 +1,39 @@
-console.log('hello');
-function greeter(person) {
-    return "hello, " + person;
+var text = "100";
+var text2;
+text2 = parseInt(text) + 100;
+console.log(text + text2);
+var hocvien = ["sac", "ads", "ewre"];
+console.log(hocvien[2]);
+for (var i = 0; i < hocvien.length; i++) {
+    console.log(hocvien[i]);
 }
-var user = 'nhixd';
-document.body.innerHTML = greeter(user);
+// enum
+var Status;
+(function (Status) {
+    Status[Status["start"] = 10] = "start";
+    Status[Status["working"] = 100] = "working";
+    Status[Status["done"] = 101] = "done";
+})(Status || (Status = {}));
+console.log(Status);
+console.log(Status[0]);
+console.log(Status["done"]);
+// tuple
+var tuple = ["string", 1, "string1"];
+console.log("🚀 ~ file: 1.ts ~ line 27 ~ tuple", tuple);
+// any
+var x = {
+    name: 'name',
+    age: 13
+};
+console.log("🚀 ~ file: 1.ts ~ line 30 ~ x", typeof x, x);
+// void
+function consolelog() {
+    console.log('this is type void');
+}
+consolelog();
+// ep kieu
+var kieu1 = 100;
+console.log("🚀 ~ file: 1.ts ~ line 45 ~ kieu1", kieu1.length);
+var a = 100;
+var b = 10;
+console.log(b.toString() + a);
--- 1.ts
+++ 1.ts
@@ -1,9 +1,49 @@
-console.log('hello');
+let text: string = "100";
+let text2: Number;
 
-function greeter(person) {
-    return "hello, " + person
+text2 = parseInt(text) + 100;
+
+console.log(text + text2);
+
+var hocvien: string[] = ["sac", "ads", "ewre"];
+console.log(hocvien[2]);
+
+for (var i = 0; i < hocvien.length; i++) {
+  console.log(hocvien[i]);
 }
 
-var user = 'nhixd'
+// enum
+enum Status {
+  start = 10,
+  working = 100,
+  done,
+}
+console.log(Status);
+console.log(Status[0]);
+console.log(Status["done"]);
 
-document.body.innerHTML = greeter(user)
+// tuple
+var tuple: [string, number, string] = ["string", 1, "string1"];
+console.log("🚀 ~ file: 1.ts ~ line 27 ~ tuple", tuple);
+
+// any
+var x: any = {
+    name: 'name',
+    age: 13,
+};
+console.log("🚀 ~ file: 1.ts ~ line 30 ~ x", typeof x, x);
+
+// void
+function consolelog():void {
+    console.log('this is type void');
+}
+
+consolelog()
+
+// ep kieu
+let kieu1 : any = 100
+console.log("🚀 ~ file: 1.ts ~ line 45 ~ kieu1", (kieu1 as string).length)
+
+let a = 100
+let b = 10
+console.log(b.toString() + a);
