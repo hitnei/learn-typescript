856081e0573e3ac0ab7404d070311c267ed30bb9
nhi-hp <77150808+nhi-hp@users.noreply.github.com>
2021-05-15 21:55:05 +0700

    hello world

---
+++ 1.js
@@ -0,0 +1,6 @@
+console.log('hello');
+function greeter(person) {
+    return "hello, " + person;
+}
+var user = 'nhixd';
+document.body.innerHTML = greeter(user);
---
+++ 1.ts
@@ -0,0 +1,9 @@
+console.log('hello');
+
+function greeter(person) {
+    return "hello, " + person
+}
+
+var user = 'nhixd'
+
+document.body.innerHTML = greeter(user)
